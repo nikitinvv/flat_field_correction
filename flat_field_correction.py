e6ecf640be2457bb415548dc22b1c0972b29d841
math-vrn <vnikitin90@gmail.com>
2021-08-01 17:36:53 -0500

    working version

--- flat_field_correction.py
+++ flat_field_correction.py
@@ -2,36 +2,44 @@ import dxchange
 import numpy as np
 import h5py
 import sys
-import skimage.feature
-import concurrent.futures as cf
-import threading
-import numpy as np
-from functools import partial
-from skimage.registration import phase_cross_correlation        
+import os
+import cupy as cp
+import cv2
+from itertools import islice
 
-proj_name = sys.argv[1]
-flat_name = sys.argv[2]
+def chunk(iterable, size):
+    it = iter(iterable)
+    item = list(islice(it, size))
+    while item:
+        yield np.array(item)
+        item = list(islice(it, size))
 
-def apply_shift(self, psi, p):
-        """Apply shift for all projections."""
-        [nz,n] = psi.shape[1:3]
-        tmp = cp.zeros([psi.shape[0], 2*nz, 2*n], dtype='float32')
-        tmp[:, nz//2:3*nz//2, n//2:3*n//2] = psi
-        [x, y] = cp.meshgrid(cp.fft.rfftfreq(2*n),
-                             cp.fft.fftfreq(2*nz))
-        shift = cp.exp(-2*cp.pi*1j *
-                       (x*p[:, 1, None, None]+y*p[:, 0, None, None]))
-        res0 = cp.fft.irfft2(shift*cp.fft.rfft2(tmp))
-        res = res0[:, nz//2:3*nz//2, n//2:3*n//2]
-        return res
+def apply_shift(proj, p):
+    """Apply shift for a projections."""
+    [ntheta, nz, n] = proj.shape
+    tmp = cp.zeros([ntheta, 2*nz, 2*n], dtype='float32')
+    idsx = cp.arange(0,2*n)
+    idsy = cp.arange(0,2*nz)
+    idsx = cp.minimum(cp.maximum(idsx-n//2,0),3*n//2-1)
+    idsy = cp.minimum(cp.maximum(idsy-nz//2,0),3*nz//2-1)
+    [idsx,idsy] = cp.meshgrid(idsx,idsy)
+    tmp = proj[:,idsy,idsx]
+    [x, y] = cp.meshgrid(cp.fft.rfftfreq(2*n),
+                         cp.fft.fftfreq(2*nz))
+    shift = cp.exp(-2*cp.pi*1j *
+                   (x*p[:, 1, None, None]+y*p[:, 0, None, None]))
+    res0 = cp.fft.irfft2(shift*cp.fft.rfft2(tmp))
+    res = res0[:, nz//2:3*nz//2, n//2:3*n//2]
+    return res
 
-def apply_shift_batch(self, u, p, ptheta=16):
-    [ntheta,nz,n] = u.shape
+
+def apply_shift_batch(proj, p):
+    """Apply shift for all projections."""
+    [ntheta, nz, n] = proj.shape
     res = np.zeros([ntheta, nz, n], dtype='float32')
-    for k in range(0, ntheta//ptheta):
-        ids = np.arange(k*ptheta, (k+1)*ptheta)
+    for ids in chunk(range(ntheta),8):        
         # copy data part to gpu
-        u_gpu = cp.array(u[ids])
+        u_gpu = cp.array(proj[ids])
         p_gpu = cp.array(p[ids])
         # Radon transform
         res_gpu = apply_shift(u_gpu, p_gpu)
@@ -39,107 +47,135 @@ def apply_shift_batch(self, u, p, ptheta=16):
         res[ids] = res_gpu.get()
     return res
 
-def _upsampled_dft(self, data, ups,
-                    upsample_factor=1, axis_offsets=None):
 
-    im2pi = 1j * 2 * np.pi
-    tdata = data.copy()
-    kernel = (cp.tile(cp.arange(ups), (data.shape[0], 1))-axis_offsets[:, 1:2])[
-        :, :, None]*cp.fft.fftfreq(data.shape[2], upsample_factor)
-    kernel = cp.exp(-im2pi * kernel)
-    tdata = cp.einsum('ijk,ipk->ijp', kernel, tdata)
-    kernel = (cp.tile(cp.arange(ups), (data.shape[0], 1))-axis_offsets[:, 0:1])[
-        :, :, None]*cp.fft.fftfreq(data.shape[1], upsample_factor)
-    kernel = cp.exp(-im2pi * kernel)
-    rec = cp.einsum('ijk,ipk->ijp', kernel, tdata)
+def find_min_max(flat):
+    """Find min and max values according to histogram"""
+    h, e = np.histogram(flat[:], 1000)
+    stend = np.where(h > np.max(h)*0.005)
+    st = stend[0][0]
+    end = stend[0][-1]
+    mmin = e[st]
+    mmax = e[end+1]
+    return mmin, mmax
 
-    return rec
 
-def registration_shift(self, src_image, target_image, upsample_factor=1, space="real"):
+def register_shift_sift(proj, flat):
+    """Find shifts via SIFT detecting features"""
+    mmin, mmax = find_min_max(flat)
+    sift = cv2.xfeatures2d.SIFT_create()
+    shifts = np.zeros([proj.shape[0], 2], dtype='float32')
+    for id in range(proj.shape[0]):
+        tmp1 = ((proj[id]-mmin) /
+                (mmax-mmin)*255)
+        tmp1[tmp1 > 255] = 255
+        tmp1[tmp1 < 0] = 0
+        tmp2 = ((flat-mmin) /
+                (mmax-mmin)*255)
+        tmp2[tmp2 > 255] = 255
+        tmp2[tmp2 < 0] = 0
+        # find key points
+        tmp1 = tmp1.astype('uint8')
+        tmp2 = tmp2.astype('uint8')
 
-    # assume complex data is already in Fourier space
-    if space.lower() == 'fourier':
-        src_freq = src_image
-        target_freq = target_image
-    # real data needs to be fft'd.
-    elif space.lower() == 'real':
-        src_freq = cp.fft.fft2(src_image)
-        target_freq = cp.fft.fft2(target_image)
+        kp1, des1 = sift.detectAndCompute(tmp1, None)
+        kp2, des2 = sift.detectAndCompute(tmp2, None)
+        # cv2.imwrite('original_image_right_keypoints.png',
+        #             cv2.drawKeypoints(tmp1, kp1, None))
+        # cv2.imwrite('original_image_left_keypoints.png',
+        #             cv2.drawKeypoints(tmp2, kp2, None))
+        match = cv2.BFMatcher()
+        matches = match.knnMatch(des1, des2, k=2)
+        good = []
+        for m, n in matches:
+            if m.distance < 0.5*n.distance:
+                good.append(m)
 
-    # Whole-pixel shift - Compute cross-correlation by an IFFT
-    shape = src_freq.shape
-    image_product = src_freq * target_freq.conj()
-    cross_correlation = cp.fft.ifft2(image_product)
-    A = cp.abs(cross_correlation)
-    maxima = A.reshape(A.shape[0], -1).argmax(1)
-    maxima = cp.column_stack(cp.unravel_index(maxima, A[0, :, :].shape))
+        draw_params = dict(matchColor=(0, 255, 0),
+                           singlePointColor=None,
+                           flags=2)
+        tmp3 = cv2.drawMatches(tmp1, kp1, tmp2, kp2, good, None, **draw_params)
+        #cv2.imwrite("original_image_drawMatches.jpg", tmp3)
+        src_pts = np.float32(
+            [kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32(
+            [kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        shift = (src_pts-dst_pts)[:, 0, :]
+        shifts[id] = np.median(shift, axis=0)[::-1]
+        print(
+            f'number of matched points for proj {id}: {len(good)}, found shifts:{shifts[id]}')
+        if(len(good)==0):
+            print(f'set shift to 0')
+            shifts[id] = 0
 
-    midpoints = np.array([cp.fix(axis_size / 2)
-                            for axis_size in shape[1:]])
-
-    shifts = cp.array(maxima, dtype=cp.float64)
-    ids = cp.where(shifts[:, 0] > midpoints[0])
-    shifts[ids[0], 0] -= shape[1]
-    ids = cp.where(shifts[:, 1] > midpoints[1])
-    shifts[ids[0], 1] -= shape[2]
-    print(shifts)
-    
-    if upsample_factor > 1:
-        # Initial shift estimate in upsampled grid
-        shifts = np.round(shifts * upsample_factor) / upsample_factor
-        upsampled_region_size = np.ceil(upsample_factor * 1.5)
-        # Center of output array at dftshift + 1
-        dftshift = np.fix(upsampled_region_size / 2.0)
-
-        normalization = (src_freq[0].size * upsample_factor ** 2)
-        # Matrix multiply DFT around the current shift estimate
-
-        sample_region_offset = dftshift - shifts*upsample_factor
-        cross_correlation = _upsampled_dft(image_product.conj(),
-                                                upsampled_region_size,
-                                                upsample_factor,
-                                                sample_region_offset).conj()
-        cross_correlation /= normalization
-        # Locate maximum and map back to original pixel grid
-        A = cp.abs(cross_correlation)
-        maxima = A.reshape(A.shape[0], -1).argmax(1)
-        maxima = cp.column_stack(
-            cp.unravel_index(maxima, A[0, :, :].shape))
-
-        maxima = cp.array(maxima, dtype=cp.float64) - dftshift
-
-        shifts = shifts + maxima / upsample_factor
-            
     return shifts
 
-def registration_shift_batch(self, u, w, ptheta=16, upsample_factor=1, space="real"):
-    res = np.zeros([ntheta, 2], dtype='float32')    
-    for k in range(0, ntheta//ptheta):
-        ids = np.arange(k*ptheta, (k+1)*ptheta)
-        # copy data part to gpu
-        u_gpu = cp.array(u[ids])
-        w_gpu = cp.array(w[ids])
-        # Radon transform
-        res_gpu = registration_shift(
-            u_gpu, w_gpu, upsample_factor, space)
-        # copy result to cpu
-        res[ids] = res_gpu.get()
-    return res
-
-def find_flat_shifts(proj, flat):
-    flat_part = flat[0:16,0:200,0:200].astype('float32')
-    proj_part = proj[0:16,0:200,0:200].astype('float32')
-    shifts = registration_shift_batch(flat_part,proj_part)
-    print(shifts)
-    dxchange.write_tiff(proj_part/(flat_part+1e-3),'/data/staff/tomograms/vviknik/tmp/fproj_part'overwrite=True)
-    dxchange.write_tiff(proj_part,'/data/staff/tomograms/vviknik/tmp/proj_part'overwrite=True)
-    dxchange.write_tiff(flat_part,'/data/staff/tomograms/vviknik/tmp/flat_part'overwrite=True)
 
 if __name__ == "__main__":
-    proj_fid = h5py.File(proj_name,'r')
-    flat_fid = h5py.File(flat_name,'r')
-    flat = flat_fid[:]
-    for k in range(proj_fid.shape[0]//chunk):
-        proj = proj_fid[k*chunk:(k+1)*chunk]        
-        shifts = find_flat_shifts(proj, flat)
+    proj_name = sys.argv[1]
+    flat_name = sys.argv[2]
+    output_name = sys.argv[3]
+    flat_region_xstart = int(sys.argv[4])
+    flat_region_xend = int(sys.argv[5])
+    flat_region_ystart = int(sys.argv[6])
+    flat_region_yend = int(sys.argv[7])
 
+    print('->create a new h5 file with corrected projections')
+    cmd = f'cp {proj_name} {output_name}'
+    os.system(cmd)
+    print('->copy done')    
+    proj_fid = h5py.File(proj_name, 'r')
+    flat_fid = h5py.File(flat_name, 'r')
+    output_fid = h5py.File(output_name, 'r+')
+    proj = proj_fid['exchange/data']
+    flat = flat_fid['exchange/data']
+
+    output_proj = output_fid.create_dataset("/exchange/data_corrected", proj.shape,
+                                    chunks=(1, proj.shape[1], proj.shape[2]), dtype='u8')
+    output_flat = output_fid.create_dataset("/exchange/data_white_corrected", flat.shape,
+                                chunks=(1, flat.shape[1], flat.shape[2]), dtype='u8')                                   
+    del output_fid["/exchange/data_white"]
+    output_iflat = output_fid.create_dataset("/exchange/data_white", flat.shape,
+                                chunks=(1, flat.shape[1], flat.shape[2]), dtype='u8')                                                                   
+    
+    
+    shifts = register_shift_sift(flat,np.median(flat,axis=0))
+    flat_shift = apply_shift_batch(flat, -shifts).astype('uint8')    
+    # dxchange.write_tiff_stack(flat,
+    #                         '/gdata/RAVEN/vnikitin/2021-07/Sobhani/tmp/fiproj_part/f', overwrite=True)
+    # dxchange.write_tiff_stack(flat_shift,
+    #                         '/gdata/RAVEN/vnikitin/2021-07/Sobhani/tmp/fproj_part/f', overwrite=True)                               
+    # exit()
+    output_flat[:] = flat_shift[:]
+    output_iflat[:] = flat[:]    
+
+    for ids in chunk(range(proj.shape[0]),128):
+        # find flat field shifts w.r.t. each projection by using small parts without sample
+        print(f'->read data {ids[0]}-{ids[-1]}')
+        flat_part = np.median(flat_shift[:, flat_region_ystart:flat_region_yend,
+                        flat_region_xstart:flat_region_xend],axis=0)
+        proj_part = proj[ids, flat_region_ystart:flat_region_yend,
+                        flat_region_xstart:flat_region_xend]
+        print('->register shift')                     
+        shifts = register_shift_sift(proj_part, flat_part)
+        print('->read whole projections')                     
+        flat_part = np.median(flat_shift.astype('float32'),axis=0)
+        proj_part = proj[ids].astype('float32')
+        print('->apply shifts')                         
+        proj_part_shift = apply_shift_batch(proj_part, -shifts)
+        print('->apply flat field correction')                         
+        
+        fproj_part = (proj_part_shift/(flat_part+1e-5))*255
+        fproj_part[fproj_part>255] = 255
+        fproj_part = fproj_part.astype('uint8')
+        
+        #fiproj_part = proj_part/(flat_part+1e-5)
+        # dxchange.write_tiff_stack(fiproj_part,
+        #                         '/gdata/RAVEN/vnikitin/2021-07/Sobhani/tmp/fiproj_part/f', overwrite=True)
+        # dxchange.write_tiff_stack(fproj_part,
+        #                         '/gdata/RAVEN/vnikitin/2021-07/Sobhani/tmp/fproj_part/f', overwrite=True)                        
+        output_proj[ids] = fproj_part                                           
+        print('->update projections done')
+        exit()
+        
+        
+        
---
+++ run.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=2 python flat_field_correction.py /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s21_new/proj_129.h5 /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s21_new/flat_130.h5 \
+/gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s21_new/cproj_129.h5 0 512 0 144 &
+
+# CUDA_VISIBLE_DEVICES=3 python flat_field_correction.py /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s20/proj_126.h5 /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s20/flat_124.h5 \
+# /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s20/cproj_126.h5 2000 2448 0 180 &
+
+# CUDA_VISIBLE_DEVICES=4 python flat_field_correction.py /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s26/proj_151.h5 /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s26/flat_150.h5 \
+# /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s26/cproj_151.h5 0 1024 0 144 &
+
+# CUDA_VISIBLE_DEVICES=5 python flat_field_correction.py /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s26/proj_148.h5 /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s26/flat_147.h5 \
+# /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s26/cproj_148.h5 2000 2448 0 180 &
+
+# CUDA_VISIBLE_DEVICES=6 python flat_field_correction.py /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s20ethanol/proj_155.h5 /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s20ethanol/flat_152.h5 \
+# /gdata/RAVEN/vnikitin/2021-07/Sobhani/radiography/s20ethanol/cproj_155.h5 0 500 0 150 &
